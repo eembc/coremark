908ed86bae8b01da51d4463220f2d39ab5ba863d
Joseph Yiu <77114984+joseph-yiu@users.noreply.github.com>
2026-05-22 10:13:46 +0100

    Update fix for u32 printf following user feedback.

--- core_main.c
+++ core_main.c
@@ -364,7 +364,7 @@ for (i = 0; i < MULTITHREAD; i++)
                   default_num_contexts * results[0].iterations
                       / time_in_secs(total_time));
 #else
-    ee_printf("Total time (secs): %d\n", time_in_secs(total_time));
+    ee_printf("Total time (secs): %"PRIu32"\n", time_in_secs(total_time));
     if (time_in_secs(total_time) > 0)
         ee_printf("Iterations/Sec   : %"PRIu32"\n",
                   default_num_contexts * results[0].iterations
