73b43e79cfa0b9c7b04d2cc22f67861bb1fa76ac
Joseph Yiu <77114984+joseph-yiu@users.noreply.github.com>
2026-08-21 09:39:16 +0100

    Adding C macro to enable main() to be renamed

--- core_main.c
+++ core_main.c
@@ -108,15 +108,19 @@ char *mem_name[3] = { "Static", "Heap", "Stack" };
 
 */
 
+#ifndef MAIN_FUNC_NAME
+#define MAIN_FUNC_NAME main
+#endif
+
 #if MAIN_HAS_NOARGC
 MAIN_RETURN_TYPE
-main(void)
+MAIN_FUNC_NAME(void)
 {
     int   argc = 0;
     char *argv[1];
 #else
 MAIN_RETURN_TYPE
-main(int argc, char *argv[])
+MAIN_FUNC_NAME(int argc, char *argv[])
 {
 #endif
     ee_u16       i, j = 0, num_algorithms = 0;
